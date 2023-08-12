52919c9e790ee093a896b3ed9280e7e78409e8dc
Amr-Hossam <43216915+Amr-Hossam@users.noreply.github.com>
2023-08-12 06:20:00 +0300

    Update provider.tf

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region  = "us-east-1"
-  access_key = "AKIAR2NTZNZG74W4CNVR"
-  secret_key = "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"
+  region      = "us-east-1"
+  access_key  = ""
+  secret_key  = ""
 }
