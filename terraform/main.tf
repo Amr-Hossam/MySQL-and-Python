b6b91f9f535f708746105bc5b5ea6e5aec9ef3f9
Amr-Hossam <43216915+Amr-Hossam@users.noreply.github.com>
2023-08-12 06:18:42 +0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,23 +1,20 @@
 module "network" {
-  source = "./network"
+  source                = "./network"
 }
 
 module "EkS" {
     source = "./EKS"
-    public1_subnet_id = module.network.public1_subnet_id
-    public2_subnet_id = module.network.public2_subnet_id
-    vpc_id = module.network.vpc_id
-  
+    public1_subnet_id   = module.network.public1_subnet_id
+    public2_subnet_id   = module.network.public2_subnet_id
+    vpc_id              = module.network.vpc_id
 }
 
 module "ECR" {
-    source = "./ECR"
-    
-
+    source              = "./ECR"
 }
 
 module "EC2" {
-    source = "./EC2"
-    public1_subnet_id = module.network.public1_subnet_id
-    vpc_id = module.network.vpc_id
+    source              = "./EC2"
+    public1_subnet_id   = module.network.public1_subnet_id
+    vpc_id              = module.network.vpc_id
 }
